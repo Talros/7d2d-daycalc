cb135fb3f9cf592705d79890302b54f205b6a927
Ben Sykes <sykes.benjamin.s@gmail.com>
2017-07-16 21:29:04 -0400

    Ben - cleanup the JS I broke :)

--- src/calc.js
+++ src/calc.js
@@ -1,40 +1,55 @@
 "use strict";
 (function() {
   document.addEventListener("DOMContentLoaded", function() {
+
     var currentDayEl = document.getElementById('current-day');
-    var calcBtnEl = document.getElementById('btn-calculate');
+    var formCalcEl = document.getElementById('form-calculate');
     var resetEl = document.getElementById('btn-reset');
     var daysLeftEl = document.getElementById('result-days-left');
     var messageEl = document.getElementById('result-message');
 
-    calcBtnEl.addEventListener('click', function(e) {
-      var currentDay = currentDayEl.value,
-          currentDayElClasses,
-          message,
-          messageElClasses,
-          daysLeft;
+    function remainingDays(day) {
+      if (day < 1)
+        return day;
 
+      return 7 - (day % 7);
+    }
+
+    function handleDisplayMessage(daysLeft) {
+      if (daysLeft < 1 || Number.isNaN(daysLeft)) {
+        currentDayEl.className = "INVALID";
+        messageEl.className = "";
+        return "Please enter a positive number above zero.";
+      }
+
+      if (daysLeft >= 7) {
+        currentDayEl.className = "";
+        messageEl.className = "GETREADY";
+        return "It's tonight! GET READY!";
+      }
+
+      currentDayEl.className = "";
+      messageEl.className = "";
+      return "";
+    }
+
+    function submitCalculation(e) {
+      e.preventDefault();
+
+      var currentDay = currentDayEl.value;
       currentDay = parseInt(currentDay);
 
-      if (!currentDay) {
-        daysLeft = "N/A";
-        message = "ERROR: Enter the current day and click Calculate.";
-        messageElClasses = "";
-        currentDayElClasses = "INVALID";
-      } else {
-        daysLeft = 7 - (currentDay % 7)
-        message = daysLeft === 7 ? "It's tonight! GET READY!" : "";
-        messageElClasses = "GETREADY";
-        currentDayElClasses = "";
-      }
-      
-      currentDayEl.className = currentDayElClasses;
-      daysLeftEl.innerText = daysLeft;
-      messageEl.innerText = message;
-      messageEl.className = messageElClasses;
-    });
+      daysLeftEl.innerText = remainingDays(currentDay);
+      messageEl.innerText = handleDisplayMessage(remainingDays(currentDay));;
+    }
+
+    formCalcEl.onsubmit = submitCalculation;
+
 
     resetEl.addEventListener('click', function(e) {
+      e.preventDefault();
+      currentDayEl.className = "";
+      messageEl.className = "";
       currentDayEl.value = "";
       daysLeftEl.innerText = "N/A";
       messageEl.innerText = "";
