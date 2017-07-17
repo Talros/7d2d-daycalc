e78325b866fea5b13cd82f99b8a330f0e38b8181
Ben Sykes <sykes.benjamin.s@gmail.com>
2017-07-16 20:44:25 -0400

    committing style things ...  blah blah

--- src/calc.js
+++ src/calc.js
@@ -3,15 +3,42 @@
   document.addEventListener("DOMContentLoaded", function() {
     var currentDayEl = document.getElementById('current-day');
     var calcBtnEl = document.getElementById('btn-calculate');
-    var daysLeftEl = document.getElementById('days-left');
+    var resetEl = document.getElementById('btn-reset');
+    var daysLeftEl = document.getElementById('result-days-left');
+    var messageEl = document.getElementById('result-message');
 
     calcBtnEl.addEventListener('click', function(e) {
-      var currentDay = currentDayEl.value;
+      var currentDay = currentDayEl.value,
+          currentDayElClasses,
+          message,
+          messageElClasses,
+          daysLeft;
+
       currentDay = parseInt(currentDay);
-      if (!currentDay) daysLeftEl.innerText = "Enter the current day and click Calculate.";
-      var daysLeft = 7 - (currentDay % 7)
-      var message = daysLeft === 7 ? "It's tonight! GET READY!" : 'Days Left: ' + daysLeft;
-      daysLeftEl.innerText = message;
+
+      if (!currentDay) {
+        daysLeft = "N/A";
+        message = "ERROR: Enter the current day and click Calculate.";
+        messageElClasses = "";
+        currentDayElClasses = "INVALID";
+      } else {
+        daysLeft = 7 - (currentDay % 7)
+        message = daysLeft === 7 ? "It's tonight! GET READY!" : "";
+        messageElClasses = "GETREADY";
+        currentDayElClasses = "";
+      }
+      
+      currentDayEl.className = currentDayElClasses;
+      daysLeftEl.innerText = daysLeft;
+      messageEl.innerText = message;
+      messageEl.className = messageElClasses;
     });
+
+    resetEl.addEventListener('click', function(e) {
+      currentDayEl.value = "";
+      daysLeftEl.innerText = "N/A";
+      messageEl.innerText = "";
+    });
+
   });
 })()
